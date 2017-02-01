1251525549f2d59c3d510294383694bfc92acdbf
Juan Orero <juaoregon@gmail.com>
2017-02-01 22:13:43 +0100

    Third Unit finished

--- README.md
+++ README.md
@@ -6,6 +6,13 @@
 - Install **Grunt dependencies** running `npm install --save-dev grunt` in your console
 - Install **JSHint Grunt plugin** running `npm install grunt-contrib-jshint --save-dev` in your console
 - Install **CoffeeScript Grunt plugin** running `npm install grunt-contrib-coffee --save-dev` in your console
+- Install **Sass Grunt plugin** running `npm install grunt-sass --save-dev` in your console
+- Install **Uglify Grunt plugin** running `npm install grunt-contrib-uglify --save-dev` in your console
+- Install **CSSMin Grunt plugin** running `npm install grunt-contrib-cssmin --save-dev` in your console
+- Install **Clean Grunt plugin** running `npm install grunt-contrib-clean --save-dev` in your console
+- Install **Build Grunt plugin** running `npm install grunt-html-build --save-dev` in your console
+- Install **Connect Grunt plugin** running `npm install grunt-contrib-connect --save-dev` in your console
+- Install **Load Grunt Tasks plugin** running `npm install load-grunt-tasks --save-dev` in your console
 - Run tasks by running `grunt <task_name>` in your console
 
 ##Class Curriculum
@@ -20,10 +27,10 @@
 	 - ~~Templating~~
 - **Basic Workflow Tasks**
 	 - ~~Running Static Analysis~~
-	 - Transcompiling CoffeeScript
-	 - Preprocessing with SASS
-	 - Minfying Code – Uglify and CSSMin
-	 - Running the Site in a Web Server
+	 - ~~Transcompiling CoffeeScript~~
+	 - ~~Preprocessing with SASS~~
+	 - ~~Minfying Code – Uglify and CSSMin~~
+	 - ~~Running the Site in a Web Server~~
 - **Advanced Workflow Tasks**
 	 - Developing with RequireJS
 	 - Unit Testing with Jasmine and Karma
--- gruntfile.js
+++ gruntfile.js
@@ -3,26 +3,96 @@ var fs = require('fs');
 module.exports = function(grunt) {
 	'use strict';
 
+	require('load-grunt-tasks')(grunt);
+
 	grunt.initConfig({
 		jshint: {
 			files: {
 				src: ['js/**/*.js']
 			}
 		},
+		clean: ['dist/**/*'],
 		coffee: {
 			dist: {
 				files: {
-					'dist/package.js': 'coffee/**/*.coffee'
+					'dist/js/package.js': 'coffee/**/*.coffee'
 				}
 			},
 			options: {
 				sourceMap: true
 			}
+		},
+		sass: {
+			dist: {
+				files: {
+					'dist/css/styles.css': 'sass/**/*.scss'
+				}
+			},
+			options: {
+				sourceMap: true
+			}
+		},
+		uglify: {
+			dist: {
+				files: {
+					'dist/js/package.min.js': 'dist/js/**/*.js'
+				}
+			},
+			options: {
+				sourceMap: true,
+				sourceMapIn: 'dist/js/package.js.map'
+			}
+		},
+		cssmin: {
+			dist: {
+				files: {
+					'dist/css/styles.min.css': 'dist/css/**/*.css'
+				}
+			},
+			options: {
+				sourceMap: true
+			}
+		},
+		htmlbuild: {
+			dist: {
+				src: 'index.html',
+				dest: 'dist/index.html',
+				options: {
+					prefix: '',
+					relative: true,
+					scripts: {
+						'package': 'dist/js/package.min.js'
+					},
+					styles: {
+						css: 'dist/css/styles.min.css'
+					}
+				}
+			},
+			dev: {
+				src: 'index.html',
+				dest: 'dist/index.html',
+				options: {
+					prefix: '',
+					relative: true,
+					scripts: {
+						'package': 'dist/js/package.js'
+					},
+					styles: {
+						css: 'dist/css/styles.css'
+					}
+				}
+			}
+		},
+		connect: {
+			server: {
+				options: {
+					base: './dist/',
+					keepalive: true,
+					open: true
+				}
+			}
 		}
 	});
 
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-coffee');
-
-	grunt.registerTask('default', ['jshint', 'coffee']);
+	grunt.registerTask('default', ['jshint', 'clean', 'coffee', 'sass', 'uglify', 'cssmin', 'htmlbuild:dist', 'connect']);
 }
